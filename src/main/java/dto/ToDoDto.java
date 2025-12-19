14d186fec74b2790e0da4ab742851bf07a907f40
Pavel Pileka <pavelpileka@gmail.com>
2025-12-19 18:12:43 +0300

    Add @AllArgsConstructor to model and dto

--- src/main/java/dto/ToDoDto.java
+++ src/main/java/dto/ToDoDto.java
@@ -1,10 +1,12 @@
 package dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class ToDoDto {
     private Long id;
     private boolean isDone;
