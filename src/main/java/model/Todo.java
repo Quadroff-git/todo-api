9403e67124f9f057ac2eea741a4f93eec9a8cd13
Pavel Pileka <pavelpileka@gmail.com>
2025-12-23 13:19:44 +0300

    Change equality check implementation for Todo class

--- src/main/java/model/Todo.java
+++ src/main/java/model/Todo.java
@@ -12,21 +12,39 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity(name = "todo")
 public class Todo {
     @Id
     @GeneratedValue
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(name = "is_done", nullable = false)
+    @EqualsAndHashCode.Include
     private boolean isDone;
 
     @Column(name = "due_date_time", nullable = false)
     private LocalDateTime dueDateTime;
 
     @Column(nullable = false)
+    @EqualsAndHashCode.Include
     private String title;
 
+    @EqualsAndHashCode.Include
     private String description;
+
+    /**
+     * Persisting and fetching of model objects results in a loss of precision for dueDateTime
+     * which causes equals to return false.
+     * Since this kind of precision is unnecessary for this domain, we simply override the dueDateTime equals check to
+     * only have minutes precision
+     * */
+    public boolean dueDateTimeEquals(Todo other) {
+        if (this.dueDateTime == null && other.dueDateTime == null) return true;
+        if (this.dueDateTime == null || other.dueDateTime == null) return false;
+
+        return this.dueDateTime.truncatedTo(java.time.temporal.ChronoUnit.MINUTES)
+                .equals(other.dueDateTime.truncatedTo(java.time.temporal.ChronoUnit.MINUTES));
+    }
 }
