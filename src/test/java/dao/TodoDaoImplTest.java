e461803b3f47ec181570494961f4b8131e0ac3e0
Pavel Pileka <pavelpileka@gmail.com>
2025-12-24 16:21:21 +0300

    Implement testGetDueOn and testGetDue

--- src/test/java/dao/TodoDaoImplTest.java
+++ src/test/java/dao/TodoDaoImplTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.ArrayList;
@@ -304,5 +305,76 @@ public class TodoDaoImplTest {
         }
 
         assertNull(sessionFactory.fromTransaction(session -> session.find(Todo.class, todo.getId())));
+    }
+
+    @Test
+    public void testGetDueOn() {
+        TodoDao todoDao = new TodoDaoImpl(sessionFactory);
+
+        // todo1 and todo2 are supposed to be returned
+        Todo todo1 = new Todo();
+        todo1.setTitle("Test todo 1");
+        todo1.setDueDateTime(LocalDateTime.of(2025, 12, 22, 20, 45));
+
+        Todo todo2 = new Todo();
+        todo2.setTitle("Test todo 2");
+        todo2.setDueDateTime(LocalDateTime.of(2026, 1, 1, 12, 0));
+
+        Todo todo3 = new Todo();
+        todo3.setTitle("Test todo 3");
+        todo3.setDueDateTime(LocalDateTime.of(2025, 12, 22, 12, 0));
+
+        Todo todo4 = new Todo();
+        todo4.setTitle("Test todo 4");
+        todo4.setDueDateTime(LocalDateTime.of(2026, 1, 14, 12, 0));
+
+        sessionFactory.inTransaction(session -> {
+            session.persist(todo1);
+            session.persist(todo2);
+            session.persist(todo3);
+            session.persist(todo4);
+        });
+
+        List<Todo> todosDue = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.beginTransaction();
+            todosDue = todoDao.getDueOn(LocalDate.of(2025, 12, 22));
+            transaction.commit();
+        }
+
+        assertEquals(2, todosDue.size());
+        assertTrue(todosDue.contains(todo1));
+        assertTrue(todosDue.contains(todo3));
+    }
+
+    @Test
+    public void testGetDue() {
+        TodoDao todoDao = new TodoDaoImpl(sessionFactory);
+
+        int todos_created = 6;
+        int due_todos_count = 2;
+
+        List<Todo> testTodos = new ArrayList<>();
+        for (int i = 0; i < todos_created; i++) {
+            Todo todo = new Todo();
+            todo.setTitle("Test todo " + i);
+            todo.setDueDateTime(LocalDateTime.now());
+            testTodos.add(todo);
+        }
+        for (int i = 0; i < todos_created - due_todos_count; i++) {
+            testTodos.get(i).setDone(true);
+        }
+
+        sessionFactory.inTransaction(session -> {
+            testTodos.forEach(session::persist);
+        });
+
+        List<Todo> completedTodos = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.beginTransaction();
+            completedTodos = todoDao.getDue();
+        }
+
+        assertEquals(due_todos_count, completedTodos.size());
     }
 }
