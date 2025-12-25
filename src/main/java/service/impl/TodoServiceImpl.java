b0cbc549d3d0bbebbfaee7949037418ecdc33f15
Pavel Pileka <pavelpileka@gmail.com>
2025-12-25 13:58:10 +0300

    Add TodoServiceImpl
    - Add stubs for all the required methods
    - Implement create()

---
+++ src/main/java/service/impl/TodoServiceImpl.java
@@ -0,0 +1,79 @@
+package service.impl;
+
+import dao.TodoDao;
+import dto.TodoDto;
+import jakarta.transaction.Transactional;
+import mapper.TodoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import service.TodoService;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+@Service
+@Transactional
+public class TodoServiceImpl implements TodoService {
+    private TodoDao todoDao;
+
+    @Autowired
+    public TodoServiceImpl(TodoDao todoDao) {
+        this.todoDao = todoDao;
+    }
+
+    @Override
+    public TodoDto create(TodoDto todoDto) {
+        if (todoDao.get(todoDto.getId()) != null) {
+            throw new IllegalArgumentException("A todo with this id exists already. TodoDtos intented for being persisted aren't supposed to have non-null id");
+        }
+        else {
+            return TodoMapper.toDto(todoDao.create(TodoMapper.toModel(todoDto)));
+        }
+    }
+
+    @Override
+    public TodoDto get(long id) {
+        return null;
+    }
+
+    @Override
+    public List<TodoDto> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public List<TodoDto> getCompleted() {
+        return List.of();
+    }
+
+    @Override
+    public List<TodoDto> getDue() {
+        return List.of();
+    }
+
+    @Override
+    public List<TodoDto> getDueOn(LocalDate date) {
+        return List.of();
+    }
+
+    @Override
+    public List<TodoDto> getDueIn(Period period) {
+        return List.of();
+    }
+
+    @Override
+    public TodoDto update(TodoDto todoDto) {
+        return null;
+    }
+
+    @Override
+    public void markCompleted(long id) {
+
+    }
+
+    @Override
+    public void delete(TodoDto todoDto) {
+
+    }
+}
