1e26cca974242ce620ed0b2ae27241e2aec2a1ea
Pavel Pileka <pavelpileka@gmail.com>
2026-03-06 16:52:08 +0300

    Implement get in TodoDaoImpl

--- src/main/java/org/pileka/dao/impl/TodoDaoImpl.java
+++ src/main/java/org/pileka/dao/impl/TodoDaoImpl.java
@@ -1,5 +1,9 @@
 package org.pileka.dao.impl;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.pileka.dao.TodoDao;
 import org.pileka.dto.TodoSpecificationDto;
 import org.pileka.model.Todo;
@@ -8,9 +12,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -45,7 +47,31 @@ public class TodoDaoImpl implements TodoDao {
 
     @Override
     public List<Todo> get(TodoSpecificationDto specDto) {
-        return List.of();
+        // Building the criteriaQuery or at least the predicate could probably be put into a util class, but ehhh
+
+        CriteriaBuilder criteriaBuilder = getSession().getCriteriaBuilder();
+        CriteriaQuery<Todo> criteriaQuery = criteriaBuilder.createQuery(Todo.class);
+        Root<Todo> todo = criteriaQuery.from(Todo.class);
+
+        var predicates = new ArrayList<Predicate>();
+        if (specDto.getIsDone() != null) {
+            predicates.add(criteriaBuilder.equal(todo.get("isDone"), specDto.getIsDone()));
+        }
+
+        if (specDto.getDueBefore() != null) {
+            predicates.add(criteriaBuilder.lessThan(todo.get("dueDateTime"), specDto.getDueBefore()));
+        }
+
+        if (specDto.getDueAfter() != null) {
+            predicates.add(criteriaBuilder.greaterThan(todo.get("dueDateTime"), specDto.getDueAfter()));
+        }
+
+        return getSession().createQuery(
+                        criteriaQuery.where(predicates.isEmpty() ?
+                                criteriaBuilder.conjunction() :
+                                criteriaBuilder.and(predicates)
+                        )
+                ).getResultList();
     }
 
 
